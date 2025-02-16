5c5137263b86a8eff8bbbc9e9a5b972c0e0f90d1
vaibhav-katyal <vaibhavkatyal12@gmail.com>
2025-02-16 16:22:17 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -18,10 +18,10 @@ int main(){
     // inverted pyramid
 
     for(int i=n-1; i>0; i--){
-        for(int k=0; k<=n-i-1; k++){
+        for(int k=0; k<n-i-1; k++){
             printf(" ");
         }
-        for(int j=0; j<=2*i+1;j++){
+        for(int j=0; j<2*i+1;j++){
             printf("*");
         }
         printf("\n");
