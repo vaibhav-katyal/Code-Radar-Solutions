c5bd5c67e63da65711a811c49cb7144c6d9f7b59
vaibhav-katyal <vaibhavkatyal12@gmail.com>
2025-03-30 08:51:40 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,8 +1,7 @@
-char caesarCipher(char *message, int shift, char *encrypted){
+void caesarCipher(char *message, int shift, char *encrypted){
     for(int i=0; message[i]!='\0'; i++){
         char ch = message[i];
         encrypted[i] = ch+shift;
     }
 
-    return *encrypted;
 }
