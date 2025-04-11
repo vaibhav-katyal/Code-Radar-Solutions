6fc5c788a9da5f784374b22dd0a3c870af8b118a
vaibhav-katyal <vaibhavkatyal12@gmail.com>
2025-04-11 14:00:13 +0530

    Solution to Streaming Service Subscription Management

---
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+struct Subscription{
+    char name[100];
+    char subs[50];
+    int price;
+};
+
+int main(){
+    struct Subscription s[50];
+    int n;
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        scanf("%s %s %d", &s[i].name, &s[i].subs, &s[i].price);
+    }
+
+    float basic_total = 0;
+    float st_total = 0;
+    float pre_total = 0;
+
+    int basic_users = 0;
+    int st_users = 0;
+    int pre_users = 0;
+
+    for(int i=0; i<n; i++){
+        if(strcmp(s[i].subs, "Basic") == 0){
+            basic_total+= s[i].price;
+            basic_users++;
+        }
+        else if(strcmp(m[i].type, "Premium") == 0){
+            pre_total+= m[i].price;
+            st_users++;
+        }
+        else{
+            vip_total+= m[i].price;
+            pre_users++;
+        }
+    }
+
+    printf("Basic: %d Users, Revenue: %.2f; Standard: %d Users, Revenue: %.2f; Premium: %d Users, Revenue: %.2f", basic_users, basic_total, st_users, st_total, pre_users, pre_total);
+    return 0;
+}
